dc30e66c4c5f6df36eb145a1d8196e3f238afe69
MurodovI05 <145528544+MurodovI05@users.noreply.github.com>
2023-09-20 11:13:10 +0500

    Update lab2.py

--- labs/lab2.py
+++ labs/lab2.py
@@ -1,16 +1,16 @@
 #
 import math
 
-x = 0
+x = 1
 y = 2
-
+z = 3
 a_n1 = x**(1/2)-(abs(y))**(1/4)
 a_n2 = x-(math.log2(y))
 a = a_n1 / a_n2
 
-b_n1 = 
+b_n1 = ((x-1)^1/2)-(abs(z)**(1/2)-(y**(1/6))+x
 # b_n1 = math.tan(example)
 # b_n2 = math.
 # example = ((x-1))^1/2
 
-print(a)
+print (a)
