557d94372a42c8cf9c11b096ec9b165707e08c07
MurodovI05 <145528544+MurodovI05@users.noreply.github.com>
2023-09-20 11:09:08 +0500

    Update lab2.py

---
+++ labs/lab2.py
@@ -0,0 +1,16 @@
+#
+import math
+
+x = 0
+y = 2
+
+a_n1 = x**(1/2)-(abs(y))**(1/4)
+a_n2 = x-(math.log2(y))
+a = a_n1 / a_n2
+
+b_n1 = 
+# b_n1 = math.tan(example)
+# b_n2 = math.
+# example = ((x-1))^1/2
+
+print(a)
